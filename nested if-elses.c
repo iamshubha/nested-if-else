925882ecc3abac00756cf0fd4972776c317805a7
shubha <shubhabanerjeewin8@gmail.com>
2019-10-25 18:48:40 +0530

    Here we Learn nested if-else

---
+++ nested if-elses.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int i ;
+
+    printf("Enter any of 1 or 2 : ");
+    scanf("%d",&i);
+
+    if (i==1)
+    {
+        printf("You would Go to heaven\n\n");
+
+    }
+    else if (i==2)
+    {
+        printf("Well You Will Passed this year with out supply\n\n");
+    }
+    else
+    {
+        printf("Your inpur is wrong\n\n");
+    }
+    
+    
+    
+}
